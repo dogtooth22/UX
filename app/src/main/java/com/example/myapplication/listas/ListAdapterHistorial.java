c284b9aacee9c1f069a57236b22885d7082db997
Dylhan <you@example.com>
2022-11-26 18:41:01 -0300

    ojala sirva

---
+++ app/src/main/java/com/example/myapplication/listas/ListAdapterHistorial.java
@@ -0,0 +1,76 @@
+package com.example.myapplication.listas;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.QualificationActivity;
+import com.example.myapplication.R;
+import com.example.myapplication.SearchPerfilActivity;
+import com.example.myapplication.menu.MenuActivity;
+
+import java.util.List;
+
+public class ListAdapterHistorial extends RecyclerView.Adapter<ListAdapterHistorial.ViewHolder> {
+    private List<HistorialModel> ListHistorial;
+    LayoutInflater mInflater;
+    private Context context;
+
+    public ListAdapterHistorial(List<HistorialModel> itemList, Context context){
+        this.mInflater = LayoutInflater.from(context);
+        this.context = context;
+        this.ListHistorial = itemList;
+    }
+    @Override
+    public int getItemCount() {return ListHistorial.size();}
+
+    @Override
+    public ListAdapterHistorial.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = mInflater.inflate(R.layout.list_historial,null);
+        return new ListAdapterHistorial.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ListAdapterHistorial.ViewHolder holder, final int position){
+        holder.bindData(ListHistorial.get(position));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HistorialModel a=ListHistorial.get(holder.getAdapterPosition());
+                if (a.getEstado().equals("Evaluar")){
+                    Intent intent = new Intent(view.getContext(), QualificationActivity.class);
+                    view.getContext().startActivity(intent);}
+
+            }
+        });
+    }
+
+    public void setItems(List<HistorialModel> items) {ListHistorial = items;}
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imagenestado;
+        TextView Name, Job, Estado;
+
+        ViewHolder (View itemView){
+            super (itemView);
+            imagenestado = itemView.findViewById(R.id.hitorialcheckstatus);
+            Name = itemView.findViewById(R.id.Historialnombre);
+            Job = itemView.findViewById(R.id.Historialtrabajo);
+            Estado = itemView.findViewById(R.id.Historialestado);
+        }
+
+        void bindData(final HistorialModel item){
+            Name.setText(item.getNombre());
+            Job.setText(item.getTrabajo());
+            Estado.setText(item.getEstado());
+            imagenestado.setImageResource(item.getIdR());
+        }
+    }
+}
