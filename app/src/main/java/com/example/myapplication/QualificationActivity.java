e0e1b1b9a04043df7357138d80d57aef01989882
MaxinhoElProgramador <maximiniabarca@gmail.com>
2022-11-27 02:52:33 -0300

    Falta revisar el estilo oscuro (que me dio paja xd)

--- app/src/main/java/com/example/myapplication/QualificationActivity.java
+++ app/src/main/java/com/example/myapplication/QualificationActivity.java
@@ -3,18 +3,26 @@ package com.example.myapplication;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.listas.HistorialModel;
+import com.example.myapplication.menu.HistoryActivity;
+import com.google.android.material.snackbar.Snackbar;
 
 public class QualificationActivity extends AppCompatActivity {
 
     public TextView nombre, correo, numero, trabajo;
     public ImageView fotoPerfil;
     private RatingBar ratingBar;
+    private Button enviar;
+    private int encuesta = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +41,17 @@ public class QualificationActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        enviar = findViewById(R.id.calificacionEnviar);
+
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+    public void EnviarCalificacion(View view){
+        Toast.makeText(getApplicationContext(),"Se ha enviado la calificación correctamente.",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(QualificationActivity.this,HistoryActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/start/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/start/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -11,6 +12,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.menu.MenuActivity;
 import com.example.myapplication.R;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -23,12 +27,26 @@ public class LoginActivity extends AppCompatActivity {
     public void login(View view){
         EditText email = (EditText) findViewById(R.id.loginEmail);
         EditText password = (EditText) findViewById(R.id.loginPassword);
+        TextInputLayout emailLayout = findViewById(R.id.login_emailLayout);
+        TextInputLayout passLayout = findViewById(R.id.login_passlayout);
 
+        if (email.getText().toString().isEmpty()){
+            emailLayout.setError("Ingrese un correo porfavor.");
+        }
+        if (password.getText().toString().isEmpty()){
+            passLayout.setError("Ingrese una contraseña porfavor.");
+        }
         if(email.getText().toString().equals("ux@usm.cl")){
             if(password.getText().toString().equals("123456")){
                 Intent login = new Intent(LoginActivity.this, MenuActivity.class);
                 startActivity(login);
             }
+            else{
+                Snackbar.make(view,"Credenciales incorrectas.",Snackbar.LENGTH_LONG).setAction("Action",null).show();
+            }
+        }
+        else{
+            Snackbar.make(view,"Credenciales incorrectas.",Snackbar.LENGTH_LONG).setAction("Action",null).show();
         }
     }
 
